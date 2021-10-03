7a18fbae12086b5cae8138a01cce7f1b36f3457f
Maksim <maxkov112211@gmail.com>
2021-10-03 14:41:01 +0300

    add:dreamTeam repeater and depth are done

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,18 +2,31 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function createDreamTeam(members) {
+  if(!Array.isArray(members))
+  return false;
+  if(members.length==0 )
+  return false;
+  let res='';
+   let regexp=/[A-z]/;
+   members.forEach(element => {
+     if(typeof(element)=="string"){
+     let char=element.match(regexp);
+     res+=char[0];
+     }
+   });
+   if(res.length==0)
+   return false;
+   return  res.toUpperCase().split('').sort().join('');
+};
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,20 +2,31 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function repeater(str, options) {
+  let res = [];
+  for (let i = 0; i < (options.repeatTimes || 1); i++) {
+    res[i] = String(str);
+    if (options.hasOwnProperty("addition")) {
+      let resaddition = [];
+      for (let j = 0; j < (options.additionRepeatTimes || 1); j++) {
+        resaddition[j] = String(options.addition);
+      }
+      res[i] += resaddition.join(options.additionSeparator || "|");
+    }
+  }
+
+  return res.join(options.separator || "+");
+};
